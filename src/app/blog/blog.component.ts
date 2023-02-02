4a1c11d50e4de8a19c933740e2ab95fc0bf18fce
Gurpreet Kaur <gkaur5600@gmail.com>
2023-02-02 16:35:40 +0530

    second commit

---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,30 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css']
+})
+export class BlogComponent {}
+
+
+
+// const swiper = new Swiper(".mySwiper",{
+// slidesPerView: 3,
+// spaceBetween : 30,
+// slidesPerGroup: 3,
+// loop: true,
+// // loopFillGroupWithBlank: true,
+
+// pagination :{
+//   el: ".swiper-pagination",
+//   clickable: true,
+// },
+// navigation:{
+// nextEl: ".swiper-button-next",
+// prevEl: ".swiper-button-prev",
+
+// },
+
+
+// });
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, NgModule } from '@angular/core';
+import Swiper from 'swiper';
+
+
+
+
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {}
+
+// const swiper = new Swiper(".mySwiper", {
+//   effect: "cards",
+//   grabCursor: true,
+// });
