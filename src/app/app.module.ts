0d53d0b0d6dbaf94af0875cf7d1b383d8cbfd746
Gurpreet Kaur <gkaur5600@gmail.com>
2023-02-02 16:26:31 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,42 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BlogComponent } from './blog/blog.component';
+import { SectionComponent } from './section/section.component';
+import { FooterComponent } from './footer/footer.component';
+import { SliderComponent } from './slider/slider.component';
 
+
+
+const routes: Routes = [
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'section', component: SectionComponent },
+  { path: 'footer', component: FooterComponent },
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BlogComponent,
+    SectionComponent,
+    FooterComponent,
+    SliderComponent,
+
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,  
+
+
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
